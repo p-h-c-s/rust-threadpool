f18d631786754e36b26de88f164d100e49df02da
p-h-c-s <pedroceravolo@jusbrasil.com.br>
2024-07-11 18:08:54 -0400

    Plain scope impl

--- src/sync_collection/thread_pool.rs
+++ src/sync_collection/thread_pool.rs
@@ -1,6 +1,6 @@
-use std::sync::{Arc, Condvar, Mutex};
+use std::{sync::{Arc, Condvar, Mutex}, thread::ScopedJoinHandle};
 use super::synchronized_queue::SynchronizedQueue;
-use crossbeam::thread::{self, Scope};
+use std::thread;
 
 // Todo: benchmark
 // Revisar static lifetime
@@ -13,39 +13,54 @@ use crossbeam::thread::{self, Scope};
 // Além disso, os objetos captados pelas closures devem existir ao menos o mesmo lifetime da Threadpool
 // Unwrapping locked Mutexes is "safe" because we should panic if a single thread panic due to mutex poisoning risks
 
+// deal with panics -> with poisoned threads/mutexes
+// do we need to keep pool?
+
 type Job<'a> = Box<dyn FnOnce() + Send + 'a>;
 
-pub struct ThreadPool<'a, 'scope>{
-    pool: Vec<thread::ScopedJoinHandle<'a, ()>>,
-    task_queue: Arc<SynchronizedQueue<Job<'a>>>,
-    server_break_sign: (Mutex<bool>, Condvar),
-    t_scope: &'a thread::Scope<'scope>
+// 'env pode ser maior que 'scope (variaveis escapam do scope)
+pub struct ThreadPool<'scope, 'env>{
+    pool: Vec<thread::ScopedJoinHandle<'scope, ()>>,
+    task_queue: Arc<SynchronizedQueue<Job<'scope>>>,
+    server_break_sign: Arc<(Mutex<bool>, Condvar)>,
+    t_scope: &'scope thread::Scope<'scope, 'env>
 }
 
-impl <'a, 'scope> ThreadPool<'a, 'scope> {
-    pub fn new(num_threads: usize, t_scope: &'a thread::Scope<'scope>) -> Self {
+impl <'scope, 'env> ThreadPool<'scope, 'env> {
+    pub fn new(num_threads: usize, t_scope: &'scope thread::Scope<'scope, 'env>) -> Self {
         ThreadPool {
             pool: Vec::with_capacity(num_threads),
             task_queue: Arc::new(SynchronizedQueue::new()),
-            server_break_sign: (Mutex::new(false), Condvar::new()),
+            server_break_sign: Arc::new(
+                (Mutex::new(false), Condvar::new())
+            ),
             t_scope
         }
     }
 
     pub fn collect_server(&mut self) {
-        let (should_break, cvar) = &self.server_break_sign;
         for _ in 0..self.pool.capacity() {
-            let task_q_ref  =  &self.task_queue;
-            self.t_scope.spawn(
-                move |scope| {
-                    let x = 2;
+            let server_break_sign = Arc::clone(&self.server_break_sign);
+            let task_q_ref  =  Arc::clone(&self.task_queue);
+            self.t_scope.spawn(move || {
+                let (should_break, cvar) = &*server_break_sign;
+                loop {
+                    let mut should_break_ref = should_break.lock().unwrap();
+                    match &*should_break_ref {
+                        false => {
+                            should_break_ref = cvar.wait_while(should_break_ref, |sb| *sb).unwrap();
+                            drop(should_break_ref);
+                            task_q_ref.pop_wait()();
+                        },
+                        true => break
+                    }
                 }
-            );
+            });
         }
     }
 
     pub fn stop_server(&mut self) {
-        let (should_break, cvar) = &self.server_break_sign;
+        let (should_break, cvar) = &*self.server_break_sign;
         *should_break.lock().unwrap() = false;
         cvar.notify_all();
     }
@@ -53,23 +68,34 @@ impl <'a, 'scope> ThreadPool<'a, 'scope> {
 
     // TODO maybe implement bulk-submit to then use notify-all
     pub fn submit<F>(&mut self, func: F)
-    where F: FnOnce() + Send + 'a
+    where F: FnOnce() + Send + 'env
     {
         self.task_queue.push(Box::new(func));
+        if self.pool.len() < self.pool.capacity() {
+            self.pool.push(
+                self.spawn_worker()
+            )
+        }
     }
 
-    pub fn collect(&mut self)
-    where 'a: 'scope
-    {
-        for _ in 0..self.pool.capacity() {
-            let task_q_ref  =  Arc::clone(&self.task_queue);
-            self.t_scope.spawn(
-                move |_| {
-                    loop {
-                        task_q_ref.pop();
+    fn spawn_worker(&self) -> ScopedJoinHandle<'scope, ()> {
+        let task_q_ref  =  Arc::clone(&self.task_queue);
+        self.t_scope.spawn(
+            move || {
+                loop {
+                    match task_q_ref.pop() {
+                        Some(task) => task(),
+                        None => break
                     }
                 }
-            );
+            }
+        )
+    }
+
+    pub fn collect(& mut self)
+    {
+        for _ in 0..self.pool.capacity() {
+            self.spawn_worker();
         }
     }
 
@@ -77,21 +103,27 @@ impl <'a, 'scope> ThreadPool<'a, 'scope> {
 }
 
 
-struct Test<'a, 'b> {
-    sc: &'a Scope<'b>
-}
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
     use super::*;
 
     #[test]
     fn test_new() {
-        let num_threads = 5;
+        let num_threads = 10;
+        let owned_str = String::from("123");
 
         thread::scope(|s| {
-            let t_pool = ThreadPool::new(num_threads, s);
-            
+            let mut t_pool = ThreadPool::new(num_threads, s);
+            for _ in 1..10 {
+                t_pool.submit(|| {
+                    println!("123");
+                    thread::sleep(Duration::from_millis(1000));
+                    println!("after");
+                });
+            }
             assert!(t_pool.pool.capacity() == num_threads);
         });
 
